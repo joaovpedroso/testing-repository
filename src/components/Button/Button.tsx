88700272adf20e258aba31d30ee24481bc97eb6a
joao.pedroso <joao.pedroso@fretebras.com.br>
2023-02-13 19:34:57 -0300

    feat: add more color white in project

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,14 +3,13 @@ import styled from "styled-components";
 export const Button = styled.button`
   cursor: pointer;
   background: var(--medium);
-  color: var(--light);
+  color: var(--white);
   padding: 8px;
   border-radius: 4px;
   width: 200px;
   border: 2px solid var(--light);
 
   &:hover {
-    color: var(--medium);
     background: var(--light);
     border: 2px solid var(--medium);
   }
